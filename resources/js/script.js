26b4d0cd7971197d497208fef1da083ac1e8ebb8
nurulasad030 <nurulasad030@gmail.com>
2020-09-27 23:04:05 +0600

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+var mixer = mixitup('.grid-portfolio-container');
+
+// hamberger menu
+
+function openMenu(){
+	document.getElementById('navbar').style.height ="100vh";
+}
+
+function closeMenu(){
+	document.getElementById('navbar').style.height ="0vh";
+}
